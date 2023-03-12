a8e2fe146bfb80809d6cbda0c2f004b7b6db9794
navidAbbasian <abbasian.navid@gmail.com>
2023-03-12 21:46:50 +0330

    All-About-Authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,5 +31,13 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
         Schema::defaultStringLength(191);
+
+        Gate::define('admin', function (User $user){
+            return $user->username == 'navan';
+        });
+
+        Blade::if('admin', function (){
+            return request()->user()?->can('admin');
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,15 @@ Route::get('login' , [SessionsController::class, 'create'])->middleware('guest')
 Route::Post('login' , [SessionsController::class, 'store'])->middleware('guest');
 Route::post('logout' , [SessionsController::class, 'destroy'])->middleware('auth');
 
-Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin');
-Route::post('admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
-Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
-Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
-Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
-Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
+Route::middleware('can:admin')->group(function (){
+    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
+    Route::post('admin/posts', [AdminPostController::class, 'store']);
+    Route::get('admin/posts', [AdminPostController::class, 'index']);
+    Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
+    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
+    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+});
+
+
 
 Route::post('newsletter',function (){return redirect('/')->with('success','Coming Soon We will fix it');});
